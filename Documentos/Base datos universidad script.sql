26c1208790abf7ee91f000ca9fb0e525775d9c76
Felipe <Felipe@172.25.14.199>
2016-05-15 14:50:45 -0500

    Base de datos corregida 2

--- Documentos/Base datos universidad script.sql
+++ Documentos/Base datos universidad script.sql
@@ -15,19 +15,32 @@ CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
 -- -----------------------------------------------------
+-- Table `mydb`.`Employee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Employee` (
+  `idEmployee` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `hiringDate` DATE NOT NULL,
+  `type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idEmployee`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `mydb`.`Student`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Student` (
-  `idStudent` INT NOT NULL,
   `type` VARCHAR(45) NOT NULL,
   `department` VARCHAR(45) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `hiringdate` DATE NOT NULL,
-  `Studentcol` VARCHAR(45) NOT NULL,
   `courses` INT NOT NULL,
   `weekHours` INT NOT NULL,
-  `salary` DOUBLE NOT NULL,
-  PRIMARY KEY (`idStudent`))
+  `Employee_idEmployee` INT NOT NULL,
+  INDEX `fk_Student_Employee1_idx` (`Employee_idEmployee` ASC),
+  CONSTRAINT `fk_Student_Employee1`
+    FOREIGN KEY (`Employee_idEmployee`)
+    REFERENCES `mydb`.`Employee` (`idEmployee`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -35,16 +48,19 @@ ENGINE = InnoDB;
 -- Table `mydb`.`Professor`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Professor` (
-  `idProfessor` INT NOT NULL,
   `type` VARCHAR(45) NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `hiringDate` DATE NOT NULL,
   `school` VARCHAR(45) NOT NULL,
   `department` VARCHAR(45) NOT NULL,
-  `salary` DOUBLE NOT NULL,
+  `salary` FLOAT NOT NULL,
   `category` VARCHAR(45) NOT NULL,
   `classHours` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idProfessor`))
+  `Employee_idEmployee` INT NOT NULL,
+  INDEX `fk_Professor_Employee_idx` (`Employee_idEmployee` ASC),
+  CONSTRAINT `fk_Professor_Employee`
+    FOREIGN KEY (`Employee_idEmployee`)
+    REFERENCES `mydb`.`Employee` (`idEmployee`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
